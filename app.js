5318cf928c281e3aba3601a8621399f5985283f3
Anton Lebedev <sorrint@gmail.com>
2022-07-02 14:15:16 +0800

    new features for menu

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const soundImage = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="32px" height="32px" viewBox="0 0 32 32" style="enable-background:new 0 0 32 32;" xml:space="preserve"> <path class="linesandangles_een" d="M26,3.674L10,8.246v13.933c-0.383-0.135-0.797-0.215-1.231-0.215c-0.342,0-0.693,0.045-1.045,0.139c-1.867,0.5-3.033,2.203-2.604,3.804c0.348,1.299,1.639,2.131,3.112,2.131c0.342,0,0.693-0.045,1.045-0.139C10.792,27.492,11.886,26.292,12,25l0,0V13.754l12-3.429v7.853c-0.383-0.135-0.797-0.215-1.231-0.215c-0.342,0-0.693,0.045-1.045,0.139c-1.867,0.5-3.033,2.203-2.604,3.804c0.348,1.299,1.639,2.131,3.112,2.131c0.342,0,0.693-0.045,1.045-0.139C24.792,23.492,25.886,22.292,26,21l0,0V3.674z M9.789,25.279c-0.219,0.32-0.595,0.571-1.031,0.687c-0.176,0.047-0.353,0.071-0.528,0.071c-0.583,0-1.079-0.273-1.18-0.649c-0.069-0.258,0.049-0.505,0.16-0.667c0.219-0.32,0.595-0.571,1.031-0.687c0.176-0.047,0.353-0.071,0.528-0.071c0.583,0,1.079,0.273,1.18,0.649C10.018,24.869,9.9,25.117,9.789,25.279z M12,11.674v-1.92l12-3.429v1.92L12,11.674z M23.789,21.279c-0.219,0.32-0.595,0.571-1.031,0.687c-0.176,0.047-0.353,0.071-0.528,0.071c-0.583,0-1.079-0.273-1.18-0.649c-0.069-0.258,0.049-0.505,0.16-0.667c0.219-0.32,0.595-0.571,1.031-0.687c0.176-0.047,0.353-0.071,0.528-0.071c0.583,0,1.079,0.273,1.18,0.649C24.018,20.869,23.9,21.117,23.789,21.279z"/></svg>'
+
 class roundMenu {
   #modules;
   #outerCircleDiameter;
@@ -39,15 +41,19 @@ class roundMenu {
     const renderModuleSize = Math.round(this.#outerCircleDiameter / 6);
     const offsetModule = this.#outerCircleDiameter / 2 - renderModuleSize / 2;
     const modulesSizeCircle = this.#outerCircleDiameter / 2 - 5;
-    console.log(renderModuleSize);
+    const modulesColor = randomColor();
 
     for (let i = 0; i < this.#modules.length; i++) {
       let renderModule = document.createElement("div");
       renderModule.className = "module";
-      renderModule.textContent = this.#modules[i];
       renderModule.style.width = renderModuleSize + "px";
       renderModule.style.height = renderModuleSize + "px";
 
+      let moduleText = document.createElement('div');
+      moduleText.className = this.#modules[i];
+      moduleText.innerHTML = soundImage;
+
+      moduleText.style.fill= modulesColor;
       let angleEachModuleRadians = (2 / this.#modules.length) * i * Math.PI - Math.PI;
 
 
@@ -56,6 +62,8 @@ class roundMenu {
       let top = modulesSizeCircle * Math.cos(angleEachModuleRadians) + offsetModule + "px";
       renderModule.style.left = left;
       renderModule.style.top = top;
+
+      renderModule.append(moduleText);
       outerCircle.append(renderModule);
     }
 
@@ -68,6 +76,11 @@ class roundMenu {
         angle = angle + 360 / this.#modules.length;
         outerCircle.style.transform = `rotate(${angle}deg)`;
       }
+      for (let i = 0; i<this.#modules.length;i++){
+        const moduleInner = document.querySelector(`.${this.#modules[i]}`);
+        moduleInner.style.transform = `rotate(-${angle}deg)`
+
+      }
     });
   }
 }
@@ -75,11 +88,12 @@ class roundMenu {
 const newMenu = new roundMenu(300);
 newMenu.render();
 
-const newMenu2= new roundMenu(360);
-newMenu2.render()
+// const newMenu2= new roundMenu(360);
+// newMenu2.render()
 
-const newMenu3 =new roundMenu(300);
-newMenu3.render();
+// const newMenu3 =new roundMenu(300);
+// newMenu3.render();
+
 function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 }
